a5febe15075be66c199989b91705796b1d137af0
Ausin O Nash <austnonash@gmail.com>
2013-01-26 13:09:34 -0600

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name     "Austin Nash"
-    email    "nash5902@stthomas.edu"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
+
+
